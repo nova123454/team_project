c2a014400f957fd8038e75a6f00e1fba96319913
nova123454 <139088342+nova123454@users.noreply.github.com>
2023-08-18 18:05:34 +0900

    2023-08-18

---
+++ js/third.js
@@ -0,0 +1,18 @@
+$(function(){
+    AOS.init({
+        duration: 1400,
+        offset : 100,
+    });
+    
+	familySite();
+    gnb();
+    fullmenu();
+    fullmenuHover();
+
+    topBtn();
+    cursor();
+    popup();
+        
+    masonry();
+});
+

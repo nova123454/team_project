1734ca22d1ee875d279a93eee075165ac1adcfd1
nova123454 <139088342+nova123454@users.noreply.github.com>
2023-08-29 17:47:43 +0900

    2023-08-29

--- js/third.js
+++ js/third.js
@@ -1,4 +1,60 @@
-$(document).ready(function(){ 
-    console.log('a')
-    $('.cover').animate({width :0},1000); 
+$(document).ready(function () {
+  console.log("a");
+  $(".cover").animate({ width: 0 }, 1000);
+});
+
+
+
+$(function(){
+
+  $('.xbox').css("transform","translateX(0px)").css("opacity","1");
+
+  
+  $(window).scroll(function(){
+
+    var ws=$(this).scrollTop();
+
+
+    var =$('.xbox').offset().top;
+
+    if(ws>-700){
+
+      $('.xbox').css("transform","translateX(0px)").css("opacity","1");
+
+    }
+
+    var =$('.ybox').offset().top;
+
+    if(ws>-700){
+
+      $('.ybox').css("transform","translateY(0px)").css("opacity","1");
+
+    }
+
+    var =$('.zbox').offset().top;
+
+    if(ws>-700){
+
+      $('.zbox').css("transform","translateY(0px)").css("opacity","1");
+
+    }
+
+    var s5=$('.tubesize').offset().top;
+
+    if(ws>-700){
+
+      $('.tubesize').css("transform","translate(0px,0px)").css("opacity","1");
+
+    }
+
+    var s6=$('.textsize').offset().top;
+
+    if(ws>-700){
+
+      $('.textsize').css("transform","translate(0px,0px)").css("opacity","1");
+
+    }
+
+  });
+
 });
